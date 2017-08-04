5f0ae7843b6ddeae538bb9c381089c09c298b549
SS-AH-IB <triode.sai@gmail.com>
2017-08-04 21:29:41 +0530

    htdtdfhjfjhfhgdhgchgc

--- app/src/main/java/com/sai/triode/saiplayer/GetMusic.java
+++ app/src/main/java/com/sai/triode/saiplayer/GetMusic.java
@@ -4,7 +4,6 @@ import android.content.ContentResolver;
 import android.database.Cursor;
 import android.net.Uri;
 import android.provider.MediaStore;
-
 import java.util.ArrayList;
 import java.util.Collections;
 
